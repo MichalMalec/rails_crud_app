6081f8833decc504235244e5cc336e1a70e2a6ce
Michal Malec <michal.malec@flatstack.com>
2020-09-30 11:22:35 +0200

    add edit template and update action

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
   get 'signup', to: 'users#new'
-  resource :users, except: [:new]
+  resources :users, except: [:new]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
