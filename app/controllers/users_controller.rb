73d988d4c7c14dfe87eff98c90bdea740e7dd724
Michal Malec <michal.malec@flatstack.com>
2020-10-16 21:42:13 +0200

    adding admin access through views and controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      sessions[:user_id] = @user.id
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to the Alpha Blog #{@user.username}, you have succesfully signed up"
       redirect_to articles_path
     else
@@ -40,7 +40,7 @@ class UsersController < ApplicationController
 
   def destroy
     @user.destroy
-    session[:user_id] = nil
+    session[:user_id] = nil if @user == current_user
     flash[:notice] = "Account and all associated articles were succesfully deleted"
     redirect_to root_path
   end
@@ -56,8 +56,8 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
-      flash[:alert] = "You can edit only your own account"
+    if current_user != @user && !current_user.admin?
+      flash[:alert] = "You can edit or delete  only your own account"
       redirect_to @user
     end
   end
