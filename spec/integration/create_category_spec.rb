737317c335e11e6656ccf52c122d31bdf4efaba0
Michal Malec <michal.malec@flatstack.com>
2020-11-06 15:36:48 +0100

    Failing scenario for create new category case

--- spec/integration/create_category_spec.rb
+++ spec/integration/create_category_spec.rb
@@ -7,10 +7,24 @@ describe "CreateCategory", type: :request do
 
     expect {
       post categories_path, params: { category: { name: "Travel" } }
-      response.should redirect_to category_path(Category.last)
+      expect(response.status).to eq 302
     }.to change(Category, :count).by(1)
     follow_redirect!
     expect(response.status).to eq 200
     assert_match "Travel", response.body
+  end
+
+  context "form validations" do
+    it "rejects invalid category if name is empty" do
+      get "/categories/new"
+      expect(response.status).to eq 200
+
+      expect {
+        post categories_path, params: { category: { name: " " } }
+      }.not_to change(Category, :count)
+      assert_match "errors", response.body
+      assert_select "div.alert"
+      assert_select "h4.alert-heading"
+    end
   end
 end
