db4837b2ff9a6edc9137dab2f07aaf2ed7bb10b4
Michal Malec <michal.malec@flatstack.com>
2020-11-23 17:50:50 +0100

    Complete category index and show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
   before_action :require_admin, except: [:index, :show]
 
   def index
@@ -20,10 +21,14 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    @category = Category.find(params[:id])
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
   def category_params
     params.require(:category).permit(:name)
